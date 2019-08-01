61f341804f640408c2d1a8e7742fb54caa772d65
BAANG <chcelestino@gmail.com>
2019-08-01 14:14:49 -0700

    Complete fridge routes; notate for future development and editting"

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,7 @@
 const db = requre('../models');
 
+// TODO: These routes are just placeholders, the queries per request will almost definitely need modifying according to the structure of our database.
+
 module.exports = {
     getFridge: function (req, res) {
         db.Fridge
@@ -16,9 +18,16 @@ module.exports = {
     },
     removeFromFridge: function (req, res) {
         db.Fridge
-        .findById({ _id: req.params.id })
-        .then(dbModel => dbModel.remove())
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err))
+            .findById({ _id: req.params.id })
+            .then(dbModel => dbModel.remove())
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err))
+    },
+    updateFridge: function (req, res) { // Update may provide better functionality when updating the list of ingredients contained within fridges. ie Load the array into this.state to render onto page => update this.state after adding or removing an item => push the new updated this.state into the database, updating the key of ingredients/products with the new one with the updated item array.
+        db.Fridge
+            .findOneandUpdate({ _id: req.params.id })
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err))
     }
+
 }
