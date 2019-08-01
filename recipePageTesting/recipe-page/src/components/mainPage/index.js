4689715cde2a46901bf571e7f5e92687c0165d06
Greyson Gerhard-Young <greyson_gerhard-young@brown.edu>
2019-08-01 12:38:07 -0700

    Added functionality to dynamically generate recipe cards based on ingredient inputs

--- recipePageTesting/recipe-page/src/components/mainPage/index.js
+++ recipePageTesting/recipe-page/src/components/mainPage/index.js
@@ -6,36 +6,58 @@ import Card from "../RecipeReviewCard"
 
 class MainPage extends Component {
     state = {
+        recipes: []
+    }
+
+    retrieveInitialRecipes = () => {
+        axios.get('https://api.spoonacular.com/recipes/findByIngredients?ingredients=bread,+lettuce,+chicken&apiKey=65fdffe480f1407ea6a4a3b80c3df511')
+            .then(res => {
+                this.setState({
+                    recipes: res.data
+                })
+                console.log("Recipes", this.state.recipes);
+            })
+            .catch(err => {
+                throw err
+            })
 
     }
 
-    // retrieveInitialRecipes = () => {
-    //     axios.get('https://api.spoonacular.com/recipes/findByIngredients?ingredients=bread,+lettuce,+chicken&apiKey=65fdffe480f1407ea6a4a3b80c3df511')
-    //         .then(res => console.log(res))
-    //         .catch(err => {
-    //             throw err
-    //         })
-
-    // }
-
-    // componentDidMount() {
-    //     this.retrieveInitialRecipes();
-    // }
+    componentDidMount() {
+        console.log("Did we get here");
+        this.retrieveInitialRecipes();
+    }
 
     render() {
-        return (
-            <div>
-                
-                <FixedContainer>
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
+        if (this.state.recipes.length) {
 
-
-                </FixedContainer>
-            </div>
-        )
+            return (
+                <div>
+                    
+                    <FixedContainer>
+                        <Card 
+                        name = {this.state.recipes[0].title}
+                        image = {this.state.recipes[0].image}
+                        missingIngredients = {this.state.recipes[0].missedIngredients}
+                        />
+                        <Card 
+                        name = {this.state.recipes[1].title}
+                        image = {this.state.recipes[1].image}
+                        missingIngredients = {this.state.recipes[1].missedIngredients}
+                        />
+                        <Card 
+                        name = {this.state.recipes[2].title}
+                        image = {this.state.recipes[2].image}
+                        missingIngredients = {this.state.recipes[2].missedIngredients}
+                        />
+                    </FixedContainer>
+                </div>
+            )
+        } else {
+            return (
+                <div></div>
+            );
+        }
     }
 
 }
