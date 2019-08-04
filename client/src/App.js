90e0c05e4fd53da07665176bd92764ca7d204a8e
BAANG <chcelestino@gmail.com>
2019-08-04 12:59:15 -0700

    Functioning navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, { Fragment, Component } from 'react';
+import { Switch, Route, Link, BrowserRouter, Redirect } from "react-router-dom";
 import { Security, ImplicitCallback } from '@okta/okta-react';
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Recipes from "./pages/Recipes";
 import Grocery from "./pages/Grocery";
 import NoMatch from "./pages/NoMatch";
+import ReactDOM from "react-dom";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
 
 const config = {
   issuer: 'https://dev-622585.okta.com/oauth2/default',
@@ -13,29 +16,68 @@ const config = {
   client_id: '0oa11poaf4Foygnfy357'
 }
 
-class App extends Component {
-  render(){
-  return (
-    <Router>
-      <div>
+// class App extends Component {
+//   render(){
+//   return (
+//     <Router>
+//       <div>
 
-      <Security issuer={config.issuer}
-                  client_id={config.client_id}
-                  redirect_uri={config.redirect_uri}
-        >
-          
-          <Switch>
-          <Route path='/' exact={true} component={Login}/>
-          <Route path='/implicit/callback' component={ImplicitCallback}/>
-          <Route exact path="/home" component={Home} />
-          <Route exact path="/recipes" component={Recipes} />
-          <Route exact path="/grocery" component={Grocery} />
-          <Route component={NoMatch} />
-        </Switch>
-        </Security>
-      </div>
-    </Router>
-  );
+//       <Security issuer={config.issuer}
+//                   client_id={config.client_id}
+//                   redirect_uri={config.redirect_uri}
+//         >
+
+//           <Switch>
+//           <Route path='/' exact={true} component={Login}/>
+//           <Route path='/implicit/callback' component={ImplicitCallback}/>
+//           <Route exact path="/home" component={Home} />
+//           <Route exact path="/recipes" component={Recipes} />
+//           <Route exact path="/grocery" component={Grocery} />
+//           <Route component={NoMatch} />
+//         </Switch>
+//         </Security>
+//       </div>
+//     </Router>
+//   );
+//   }
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+
+          <Security issuer={config.issuer}
+            client_id={config.client_id}
+            redirect_uri={config.redirect_uri}>
+
+              <Route 
+                path = "/"
+                render={({ location }) => (
+                  <Fragment>
+                    <Tabs value={location.pathname}>
+                      <Tab label="Login" value="/" component={Link} to="/" />
+                      <Tab label="Home" value="/home" component={Link} to="/home" />
+                      <Tab value="/recipes" label="Recipes" component={Link} to="/recipes"/>
+                      <Tab label="Groceries" value="/grocery" component={Link} to="/grocery" />
+                    </Tabs>
+                    <Switch>
+                      <Route exact path="/" component = {Login} />
+                      <Route exact path="/home" component = {Home} />
+                      <Route exact path="/recipes" component = {Recipes} />
+                      <Route exact path="/grocery" component = {Grocery}/>
+                      <Route exact path='/implicit/callback' component={ImplicitCallback}/>
+                    </Switch>
+                  </Fragment>
+                )}
+                />
+
+          </Security>
+
+        </div>
+      </BrowserRouter>
+    )
   }
 }
 
